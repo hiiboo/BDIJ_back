ce601dad95c1bbb6ca82d6bcf89b74c0b485cc11
Yugo <satoyugo1022@gmail.com>
2023-10-01 10:06:28 +0900

    temporary allow all origin setting

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3000/*', 'https://bestdayinjapan.vercel.app/*'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3000/*', 'https://bestdayinjapan.vercel.app/*','*'],
 
     'allowed_origins_patterns' => [],
 
